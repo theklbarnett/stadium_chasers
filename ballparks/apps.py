a9008e1287433c0f636b3fc38be0d3826bb352ba
Kyle Barnett <theklbarnett@gmail.com>
2020-05-24 09:49:41 -0700

    initialized project, first app and venv

---
+++ ballparks/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BallparksConfig(AppConfig):
+    name = 'ballparks'
---
+++ stadium_chasers/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for stadium_chasers project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stadium_chasers.settings')
+
+application = get_asgi_application()
